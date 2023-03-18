265768a247ec671534f5c21ef7214a431abd1ac9
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-03-18 13:18:54 -0400

    add initial functionality to signup page

--- src/screens/SignupPage/SignupForm.js
+++ src/screens/SignupPage/SignupForm.js
@@ -1,24 +1,108 @@
-import { Text, TextInput, View, StyleSheet,TouchableOpacity } from "react-native";
+import { Text, TextInput, View, StyleSheet,TouchableOpacity, Alert, Platform } from "react-native";
+import { useState } from "react";
 
 export default function WelcomeForm(){
+    const [enteredFullName, setEnteredFullName] = useState("");
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState("");
+    const [enteredConfirmPassword, setEnteredConfirmPassword] = useState("");
+
+    const [credentialsInvalid, setCredentialsInvalid] = useState({
+        fullName: false,
+        username: false,
+        password: false,
+        confirmPassword: false,
+    });
+
+    function updateInputValueHandler(inputType, value){
+        switch(inputType){
+            case 'name':
+                setEnteredFullName(value);
+                break;
+            case 'username':
+                setEnteredUsername(value);
+                break;
+            case 'password':
+                setEnteredPassword(value);
+                break;
+        }
+    }
+
+    function submitHandler() {
+
+        setEnteredFullName(enteredFullName.trim());
+        setEnteredUsername(enteredUsername.trim());
+        setEnteredPassword(enteredPassword.trim());
+
+        const fullNameIsInvalid = enteredFullName.trim().split(" ").length == 2;
+        const usernameIsValid = enteredUsername.length > 0;
+        const passwordIsValid = enteredPassword.length > 6;
+        const passwordsAreEqual = enteredPassword === enteredConfirmPassword;
+
+        if (
+            !fullNameIsInvalid ||
+            !usernameIsValid ||
+            !passwordIsValid
+            // || (!isLogin && (!passwordsAreEqual))
+          ) {
+            Alert.alert('Invalid input', 'Please check your entered credentials.');
+            setCredentialsInvalid({
+                fullName: !fullNameIsInvalid,
+                username: !usernameIsValid,
+                password: !passwordIsValid,
+                confirmPassword: !passwordIsValid || !passwordsAreEqual,
+            });
+            return;
+          }
+
+          /* else, authenticate user */
+        //   onAuthenticate({ email, password });
+        Alert.alert('Success!', 'User signup successful.'); //temp for now
+        setCredentialsInvalid({
+            fullName: !fullNameIsInvalid,
+            username: !usernameIsValid,
+            password: !passwordIsValid,
+            confirmPassword: !passwordsAreEqual,
+        })
+        setEnteredFullName("");
+        setEnteredUsername("");
+        setEnteredPassword("");
+        setEnteredConfirmPassword("");
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.inputContainer}>
-                <Text style={styles.label}>Full Name</Text>
-                <TextInput placeholder='Enter Full Name' style={styles.input} />
+                <Text style={[styles.label, credentialsInvalid.fullName && styles.invalidLabel]}>Full Name</Text>
+                <TextInput 
+                    placeholder='First and Last name' 
+                    style={[styles.input, credentialsInvalid.fullName && styles.invalidInput]} 
+                    onChangeText={updateInputValueHandler.bind(this, 'name')}
+                    value={enteredFullName}
+                />
             </View>
 
             <View style={styles.inputContainer}>
-                <Text style={styles.label}>Username</Text>
-                <TextInput keyboardType='email-address' autoCorrect={false} autoCapitalize='none' style={styles.input} />
+                <Text style={[styles.label, credentialsInvalid.username && styles.invalidLabel]}>Username</Text>
+                <TextInput 
+                    keyboardType='email-address' 
+                    autoCorrect={false} autoCapitalize='none' 
+                    style={[styles.input, credentialsInvalid.username && styles.invalidInput]} 
+                    onChangeText={updateInputValueHandler.bind(this, 'username')}
+                    value={enteredUsername}
+                />
             </View>
 
             <View style={styles.inputContainer}>
-                <Text style={styles.label}>Password</Text>
-                <TextInput style={styles.input} />
+                <Text style={[styles.label, credentialsInvalid.password && styles.invalidLabel]}>Password</Text>
+                <TextInput 
+                    style={[styles.input, credentialsInvalid.password && styles.invalidInput]} 
+                    onChangeText={updateInputValueHandler.bind(this, 'password')}
+                    value={enteredPassword}
+                />
             </View>
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={submitHandler}>
                 <Text style={styles.buttonText}>Sign Up</Text>
             </TouchableOpacity>
 
@@ -59,6 +143,14 @@ const styles = StyleSheet.create({
         fontSize: 18,
         borderColor: '#DBD5D5',
         borderWidth: 1,
+        height: 42,
+    },
+    invalidLabel: {
+        color: "#9b095c",
+    },
+    invalidInput: {
+        backgroundColor: "#fcc4e4",
+        borderColor: '#9b095c',
     },
     button: {
         height: 50,
@@ -71,6 +163,17 @@ const styles = StyleSheet.create({
         borderRadius: 11,
         marginBottom: 5,
         marginTop: 50,
+        ...Platform.select({
+            ios: {
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: 3 },
+                shadowOpacity: 0.2,
+                shadowRadius: 1,
+            },
+            android: {
+                elevation: 4,
+            },
+        }),
     },
     buttonText: {
         color: "white",
--- src/screens/SignupPage/SignupPage.js
+++ src/screens/SignupPage/SignupPage.js
@@ -7,13 +7,17 @@ import {
 } from "react-native";
 import SignupForm from "./SignupForm";
 import SignupImg from "./images/SignupImg.svg";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+// navigation.replace() //goes to a new page without having a back button
 
 export default function SignupPage() {
   return (
+    <KeyboardAwareScrollView>
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
         <View style={styles.imgContainer}>
-          <SignupImg style={styles.welcomeImg} />
+          <SignupImg height="100%" width="100%" style={styles.welcomeImg} />
         </View>
 
         <Text style={styles.header}>Create An Account</Text>
@@ -24,21 +28,27 @@ export default function SignupPage() {
         <SignupForm />
       </View>
     </TouchableWithoutFeedback>
+    </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#FFFFFF",
+    flex: 1,
+    height: "100%",
+    paddingBottom: 30,
   },
   imgContainer: {
     width: "100%",
+    minHeight: 200,
+    flex: 1,
+    flexShrink: 0,
     display: "flex",
     alignItems: "center",
   },
   welcomeImg: {
     width: "100%",
-    height: "100%",
   },
   header: {
     fontSize: 20,
