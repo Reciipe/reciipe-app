1196591e0e3048b223840f41ff2e5c1dddd29967
anjola85 <anjola85@gmail.com>
2023-03-25 00:15:07 -0400

    fixed reducer error with dummy reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,3 +1,12 @@
 import { combineReducers } from "redux";
 
-export const rootReducer = combineReducers({});
+const defaultReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  default: defaultReducer,
+});
